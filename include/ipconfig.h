e268a3e833cf411218666e798ebde848f426fe15
utkarshbhatthere <utkarsh_bhatt@outlook.com>
2019-06-11 05:27:25 +0530

    standard ip settings saved here.

---
+++ include/ipconfig.h
@@ -0,0 +1,8 @@
+#ifndef _IP_CONFIG_H_
+#define _IP_CONFIG_H_
+
+#define ipconfig_DEFAULT_IP         "10.10.0.1"
+#define ipconfig_DEFAULT_GATEWAY    "10.10.0.1"
+#define ipconfig_DEFAULT_NETMASK    "255.255.255.0"
+
+#endif /* @_IP_CONFIG_H_ */
